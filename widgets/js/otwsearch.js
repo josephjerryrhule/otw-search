2e4229fab265ef0dff4eac4c69d387b708f610b8
joseph <josephjerryrhule@gmail.com>
2024-05-14 14:58:31 +0000

    updated search input query

--- widgets/js/otwsearch.js
+++ widgets/js/otwsearch.js
@@ -39,31 +39,63 @@ jQuery(document).ready(function ($) {
 });
 
 jQuery(document).ready(function ($) {
-  $('.otw-search-input-container input[type="search"]').on(
-    "input",
+  var typingTimer; // Timer identifier
+  var doneTypingInterval = 500; // Time in milliseconds (0.5 seconds)
+
+  // Function to perform search when user stops typing
+  function doneTyping() {
+    // Show loading indicator
+    $(".otw-search-results-container").addClass("loading");
+
+    var searchTerm = $(
+      ".otw-search-input-container input[type='search']"
+    ).val();
+    $.ajax({
+      url: ajax_data.ajax_url,
+      type: "POST",
+      data: {
+        action: "custom_search_action",
+        search_term: searchTerm,
+      },
+      success: function (response) {
+        // Hide loading indicator
+        $(".otw-search-results-container").removeClass("loading");
+
+        // Update categories and products area with the AJAX response
+        $(".otw-search-results-container").html(response);
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    });
+  }
+
+  // Function to handle search input
+  function handleSearchInput() {
+    clearTimeout(typingTimer);
+    var searchTerm = $(
+      ".otw-search-input-container input[type='search']"
+    ).val();
+    if (searchTerm) {
+      typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    } else {
+      doneTyping(); // Perform search if search field is empty after a delay
+    }
+  }
+
+  // On keyup, start the countdown
+  $(".otw-search-input-container input[type='search']").on(
+    "keyup",
     function () {
-      // Show loading indicator
-      $(".otw-search-results-container").addClass("loading");
+      handleSearchInput();
+    }
+  );
 
-      var searchTerm = $(this).val();
-      $.ajax({
-        url: ajax_data.ajax_url, // WordPress AJAX URL
-        type: "POST",
-        data: {
-          action: "custom_search_action", // AJAX action name
-          search_term: searchTerm,
-        },
-        success: function (response) {
-          // Hide loading indicator
-          $(".otw-search-results-container").removeClass("loading");
-
-          // Update categories and products area with the AJAX response
-          $(".otw-search-results-container").html(response);
-        },
-        error: function (error) {
-          console.log(error);
-        },
-      });
+  // On keydown, clear the countdown
+  $(".otw-search-input-container input[type='search']").on(
+    "keydown",
+    function () {
+      clearTimeout(typingTimer);
     }
   );
 });
